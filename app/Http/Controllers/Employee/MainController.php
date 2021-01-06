ecd666c5894e22ebe539893faa1d0f14b37a3058
IDLKZ <idlkz2019@gmail.com>
2021-01-06 15:56:55 +0600

    Third Global Commit

--- app/Http/Controllers/Employee/MainController.php
+++ app/Http/Controllers/Employee/MainController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Employee;
 
 use App\Http\Controllers\Controller;
+use App\Models\BelbinQuiz;
+use App\Models\BelbinUser;
 use App\Models\Invite;
 use App\Models\JobMotive;
 use App\Models\Motive;
@@ -67,7 +69,7 @@ class MainController extends Controller
     public function solovievShow($id){
         $result = Result::where(["user_id"=>Auth::id()])->with(["job","user"])->find($id);
         if($result){
-            $invite = Invite::where("user_id",Auth::id())->orWhere("department_id",Auth::user()->department_id)->with(["department","type"])->find($result->invites_id);
+            $invite = Invite::where(function ($q){$q->where("user_id",Auth::id());$q->orWhere("department_id",Auth::user()->department_id);})->with(["department","type"])->find($result->invites_id);
             if($invite){
                 $meaning = UserMeaning::where("result_id",$result->id)->with(["result"])->get();
                 $motivation = UserMotivation::where("result_id",$result->id)->get();
@@ -75,7 +77,11 @@ class MainController extends Controller
                 $scales = UserScale::where("result_id",$result->id)->with("scale")->get();
                 $job_motive = JobMotive::where("job_id",$result->job_id)->get()->groupBy("motive_id")->toArray();
                 $all_motives = collect(Motive::get()->groupBy("id")->toArray());
-                return view("employee.result.soloviev-show",compact("result","meaning","motivation","motives","scales","job_motive","invite","all_motives"));
+
+                if($invite->type_id == 1 && $meaning->isNotEmpty() && $motivation->isNotEmpty() && $motives->isNotEmpty() && $scales->isNotEmpty() && count($job_motive) && count($all_motives)){
+                    return view("employee.result.soloviev-show",compact("result","meaning","motivation","motives","scales","job_motive","invite","all_motives"));
+                }
+                abort(404);
 
             }
             else{
@@ -91,6 +97,25 @@ class MainController extends Controller
     }
 
     public function belbinShow($id){
+        $result = Result::where(["user_id"=>Auth::id()])->with(["job","user"])->find($id);
+        if($result){
+            $invite = Invite::where(function ($q){$q->where("user_id",Auth::id());$q->orWhere("department_id",Auth::user()->department_id);})->with(["department","type"])->find($result->invites_id);
+             if($invite){
+                $quiz = BelbinQuiz::first();
+                $belbin_user = BelbinUser::where("result_id",$result->id)->with("belbinRole")->get();
+                if($belbin_user->isNotEmpty() && $invite->type_id == 2){
+                    return view("employee.result.belbin-show",compact("result","invite","belbin_user"));
+                }
+                else{
+                    abort(404);
+                }
+
+             }
+             abort(404);
+        }
+        else{
+            abort(404);
+        }
 
     }
 }
